349dcbbce82366a10a4effbd5c8e2c2cf8d2ada9
Dennis Campos <camposdennis37@gmail.com>
2012-08-18 12:14:20 -0600

    Se creó la actividad para el Login, todavía no hace nada

--- OAMMBLO/src/com/fr4gus/android/oammblo/ui/SplashActivity.java
+++ OAMMBLO/src/com/fr4gus/android/oammblo/ui/SplashActivity.java
@@ -1,8 +1,11 @@
 package com.fr4gus.android.oammblo.ui;
 
+import com.fr4gus.android.oammblo.Login_Activity;
 import com.fr4gus.android.oammblo.R;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 /**
  * Shows App logo for few seconds.
@@ -12,10 +15,25 @@ import android.os.Bundle;
  */
 public class SplashActivity extends OammbloActivity {
 
+	Handler mHandler = new Handler();
+	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.splash);
     }
+
+	@Override
+	protected void onResume() {
+		// TODO Auto-generated method stub
+		super.onResume();
+		mHandler.postDelayed(new Runnable() {
+			public void run(){
+				startActivity(new Intent(SplashActivity.this, Login_Activity.class));
+			}
+		}, 3000);
+	}
+    
+    
 
 }
