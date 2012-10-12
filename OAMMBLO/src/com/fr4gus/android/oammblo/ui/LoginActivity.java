779c57b9a2ea878e527d3c7c20b7e1d9eb11cdce
Frankln Garcia <fggarcia@gmail.com>
2012-10-12 16:10:06 -0600

    Removing imports

--- OAMMBLO/src/com/fr4gus/android/oammblo/ui/LoginActivity.java
+++ OAMMBLO/src/com/fr4gus/android/oammblo/ui/LoginActivity.java
@@ -4,7 +4,6 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.support.v4.app.DialogFragment;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
